19e17b9464933007a64d2e25971d01ba254cf81f
santiagobravin <santiagobravin@gmail.com>
2022-08-10 17:20:45 -0300

    formulario de registro

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { resourceLimits } = require('worker_threads');
+
+const usersFilePath = path.join(__dirname, '../data/usersData.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+
 const userController = {
     userInfo: (req,res) =>{
         // TODO : crear pagina de usuario //
@@ -10,8 +18,19 @@ const userController = {
         res.render('loginForm')
     },
     uploadUser: (req,res) =>{
-        let userInfo = req.body;
-        res.render('loginForm')
+        let newUser = {
+            
+            name: req.body.name,
+            email: req.body.email,
+            number: req.body.number,
+            password: req.body.password, 
+            id: users[users.length - 1].id + 1,
+            active: true
+        };
+        users.push(newUser);
+        let usersJSON = JSON.stringify(users, null, ' ');
+        fs.writeFileSync(usersFilePath, usersJSON);
+        res.redirect('loginForm');
     }
    
 }
