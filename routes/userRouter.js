e42b91658c9e1aba1f693082121c77de722db854
carloscm21 <ccastromaggio@yahoo.com.ar>
2022-08-16 23:36:56 -0300

    Actualizaciones en Clase 27 - implemantación de express-validator

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const {body} = require('express-validator');
+
 const router = express.Router();
 
 const multer = require('multer'); /* Requerir multer. En el form como atributo va --> (enctype = "multipart/form-data") */
@@ -10,11 +12,20 @@ const userController = require('../controllers/userController');
 
 // /users/...
 
-router.get('/my-account', userController.userInfo)
+let validations = [
+    body('name')
+    .notEmpty() .withMessage('El nombre es obligatorio'),
+    body('email')
+    .isEmail() .withMessage('Debe ingresar un email válido'),
+    body('password')
+    .isLength({min: 6}) .withMessage('La contraseña debe tener un mínimo de 6 caracteres')   
+];
+
+router.get('/my-account', userController.userInfo);
 
 router.get('/registration-form',userController.register);
 
-router.post('/registration-form',userController.uploadUser);
+router.post('/registration-form', validations, userController.uploadUser);
 
 router.get('/login-form', userController.login);
 
